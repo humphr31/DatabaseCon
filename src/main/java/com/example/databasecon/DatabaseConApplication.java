b82aaa6e870422f5fda707f0d6dbc57474806f36
humphr31 <humphr31@purdue.edu>
2022-01-22 18:08:15 -0500

    Initial commit

---
+++ src/main/java/com/example/databasecon/DatabaseConApplication.java
@@ -0,0 +1,13 @@
+package com.example.databasecon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseConApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseConApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/databasecon/DatabaseConApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.databasecon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseConApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
