3dac52adc65db0be681af071edbdb641e3d587ab
humphr31 <humphr31@purdue.edu>
2022-02-26 09:39:36 -0500

    Basic Level Entity Class and identifying repository interface. Will talk more about it in the meeting as to what these mean

---
+++ src/main/java/com/example/databasecon/customerRepository.java
@@ -0,0 +1,9 @@
+package com.example.databasecon;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.databasecon.customer;
+
+public interface customerRepository extends CrudRepository<customer, Integer>{
+
+}
